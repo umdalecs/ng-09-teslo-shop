83236829b25e8877fc6070ebed0967c965f81d4d
Alejandro Flores <marale7951@gmail.com>
2025-08-14 00:15:06 -0700

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styles: [],
+})
+export class App {
+  protected title = 'teslo-shop';
+}
